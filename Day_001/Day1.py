dc675e879f8cb98d3e30194b7313bfe8e3a0e151
Suryansh <suryanshcodeframer@gmail.com>
2026-05-16 19:45:01 +0530

    Added Day 001 Python project

---
+++ Day_001/Day1.py
@@ -0,0 +1,5 @@
+print ("welcome to the Band Name Generatyor.")
+city= input ("which city did you grow up in??")
+pet=input("What is the name of the pet ?")
+
+print("Your band name cour be :"+ city + " "+ pet)
