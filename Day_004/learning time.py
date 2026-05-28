d8dffc895133a3d02ad44eb5d787f783be68593b
Suryansh <suryanshcodeframer@gmail.com>
2026-05-28 12:47:22 +0530

    day 4 done

---
+++ Day_004/learning time.py
@@ -0,0 +1,15 @@
+import random
+# import favtNum
+#
+# print("Hello World")
+# random_integer = random.randint(1,100)
+# print(random_integer)
+#
+# print(favtNum.my_favorite_number)
+#
+h="head"
+t="tail"
+l=[h,t]
+a=random.randint(0,1)
+print(l[a])
+
---
+++ Day_004/list.py
@@ -0,0 +1,6 @@
+# who pay the bills
+import random
+
+names_list = ["Alex", "Jordan", "Taylor", "Morgan", "Sam"]
+randome_index=random.randint(0,len(names_list)-1)
+print(names_list[randome_index])
